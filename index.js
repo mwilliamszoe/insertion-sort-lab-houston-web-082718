e615051c35d7982ea2165a9784685ef7709b6d2b
Zoe Monet Williams <mwilliamszoe@gmail.com>
2019-03-11 12:16:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
-
+let currentMin = array[0]
+let minIndex = 0
+for(let i=0; i<array.length; i++){
+  if(array[i] < currentMin){
+    currentMin = array[i]
+    minIndex = i
+    }
+  }
+  array.splice(minIndex, 1);
+  return currentMin
 }
 
-function insertionSort(array){
 
+function insertionSort(array){
+  let sorted = []
+  let min;
+  while(array.length != 0){
+    min = findMinAndRemove(array)
+    sorted.push(min)
+  }
+  return sorted;
 }
