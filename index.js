18ed1207517d71446f5f726fc80f177d756b6c4b
Zoe Monet Williams <mwilliamszoe@gmail.com>
2019-03-11 12:13:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,15 +14,3 @@ for(let i=0; i<array.length; i++){
 function insertionSort(array){
 
 }
-// function findMinAndRemove(array){
-//   let currentMin = array[0]
-//   let minIndex = 0
-//   for(let i = 0; i < array.length; i++){
-//     if(array[i] < currentMin){
-//       currentMin = array[i]
-//       minIndex = i
-//     }
-//   }
-//   array.splice(minIndex, 1);
-//   return currentMin;
-// }
